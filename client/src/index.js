5ba714295a499d0f7bfdde8dc832c44e35242194
Samantha Wiley <samantha.wiley@student.csulb.edu>
2020-03-18 21:21:45 -0700

    init authentication via cache memory

--- client/src/index.js
+++ client/src/index.js
@@ -5,41 +5,61 @@ import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 import { gql } from 'apollo-boost';
 
-const POSTS = gql`
-{
-    posts {
-        id
-      user {
-        id
-      }
-      createdAt
-    }
-}
-`;
+import Home from './components/home';
+import Login from './components/login';
 
-function Posts() {
-    const { loading, error, data } = useQuery(POSTS);
+// const POSTS = gql`
+// {
+//     posts {
+//         id
+//       user {
+//         id
+//       }
+//       createdAt
+//     }
+// }
+// `;
+
+// function Posts() {
+//     const { loading, error, data } = useQuery(POSTS);
   
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+//     if (loading) return <p>Loading...</p>;
+//     if (error) return <p>Error :(</p>;
   
-    return data.posts.map(({ id, user }) => (
-      <div key={id}>
-        <p>
-          {id}: {user.id}
-        </p>
-      </div>
-    ));
-  }
+//     return data.posts.map(({ id, user }) => (
+//       <div key={id}>
+//         <p>
+//           {id}: {user.id}
+//         </p>
+//       </div>
+//     ));
+//   }
 
-
+const cache = new InMemoryCache();
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
 });
 
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+  },
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Home /> : <Login />;
+}
+
 const App = () => (
   <ApolloProvider client={client}>
-    <Posts />
+    <IsLoggedIn />
   </ApolloProvider>
 );
 
